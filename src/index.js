a5a998c9588ea7bcca1ddef78e539c0309b1af2e
Simran <70709117+simran-xyz@users.noreply.github.com>
2022-03-02 19:57:25 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 const express = require('express');
-
-const app = express();
-
-const PORT = process.env.PORT || 3539;
-const route = require("./route");
-
+const app=express();
 app.use(express.json());
-app.use(route);
 
-app.listen(PORT, () => {
-    console.log(`server is up on port ${PORT}`);
-})
+
+
+app.post("/bfhl",async(req, res) => {
+    const {data} = req.body;
+    const num = [];
+    const character = [];
+    const characterRegex = /^[a-zA-Z]{1,}$/;
+    const numberRegex = /^[0-9]{1,}$/;
+    
+        data.map((item) => {
+            if(numberRegex.test(item)) {
+                numArr.push(item);
+            } else if(characterRegex.test(item)){
+                charArr.push(item);
+            }
+        });
+    
+        const result = {
+            isSuccess: "true",
+            user_id: "simran_singla_29032002",
+            email: "simran1155.cse19@chitkara.edu.in",
+            roll_number:"1910991155" ,
+            numbers: numArr,
+            alphabets: charArr
+        };
+    
+        res.status(200).send(result);
+    
+});
+
+const port = process.env.PORT || 6001;
+app.listen(port,()=> {
+    console.log(`app running on port ${port}`);
+});
